27c16d45aa1d52127bcb9a75f9b9c413489e8db6
JoseASantoss <j.santos.sanchez@gmail.com>
2021-04-23 20:05:48 +0200

    comenzando con la sección contacto

---
+++ enobras/JS/scripts.js
@@ -0,0 +1,4 @@
+copyright = new Date();
+update = copyright.getFullYear();
+document.write("© 2021 - " + update + " " + "El sitio Web");
+//document.getElementById('date').innerHTML= ("© 2021 - " + update + " " + "El sitio Web");
