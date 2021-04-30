d5484eb94dde0b8692fbd8955528adc91c5eafd1
JoseASantoss <j.santos.sanchez@gmail.com>
2021-04-30 20:00:39 +0200

    Se termina de pulir el JS del formulario de contacto simple y se añaden comentarios y se acuerda reutilizar el máximo código posible para el formulario

--- js/form.js
+++ js/form.js
@@ -9,10 +9,32 @@ let errors = {
     texto: true,
 };
 
+/*La expresión regular de nombre establece que
+empiece por una letra mayúscula y sigan
+entre 8 y 30 caracteres, mayúsculas o minúsculas
+o _ y acabe por alguna de estas
+(Entendemos que el usuario introduce nombre y apellidos)*/
 const nomformatRegex = /^[A-Z]{1}[A-Za-z_]{2,30}$/;
 
+/*Con la expresión regular del email, pretendemos que
+sean válidos estos emails prototipo:
+    test@patata.com
+    very.common@patata.com
+    disposable.style.email.with+symbol@patata.com
+    other.email-with-hyphen@pa_tata.com
+    fully-qualified-domain@patata_2.net
+    user.name+tag+sorting@patata.org
+    user_name_test@pata_12.es
+*/
+
+/*La primera parte tenga minúsculas, números, guiones, guiones bajos,
+puntos o +, entre 3 y 25 caracteres;; arroba;; dominio con letras,
+números o _ ;; y .com/.es/.org ... */
 const mailformatRegex = /^[a-z0-9-_.+]{3,25}@[a-z0-9_]{2,12}.[a-z]{2,6}$/;
 
+/*El teléfono empezará por 6,7,8,9 que son los números
+prefijo usados en España, y tendrá otros 8 números, haciendo
+un teléfono de 9 dígitos típico de España*/
 const telformatRegex = /^[0-9/^[6|7|8|9]{1}[0-9]{8,8}$/;
 
 const caja = document.querySelectorAll('input.sp-form, textarea.sp-form');
@@ -41,26 +63,26 @@ validation = (evento) => {
 
     if(evento.name == 'nombre'){
         if(!evento.value.match(nomformatRegex)){
-            muestraError(true, evento);
+            mostrarError(true, evento);
         }else{
-            muestraError(false, evento);
+            mostrarError(false, evento);
         }
                
     } 
 
     if(evento.name == 'email'){
         if(!evento.value.match(mailformatRegex)){
-            muestraError(true, evento);
+            mostrarError(true, evento);
         }else{
-            muestraError(false, evento);
+            mostrarError(false, evento);
         }
     }
 
     if(evento.name == 'telefono'){
         if(!evento.value.match(telformatRegex)){
-            muestraError(true, evento);
+            mostrarError(true, evento);
         }else{
-            muestraError(false, evento);
+            mostrarError(false, evento);
         }
         
     }
@@ -69,11 +91,11 @@ validation = (evento) => {
         console.log(evento.value);
         if(evento.value.length < 50 || evento.value.length > 1000){
             console.log("Texto MAL, INÚTIL");
-            muestraError(true, evento);
+            mostrarError(true, evento);
 
         }else{
             console.log("Texto OK");
-            muestraError(false, evento);
+            mostrarError(false, evento);
 
         }
     }
@@ -81,7 +103,7 @@ validation = (evento) => {
     submitControlador();
 };
 
-muestraError = (check, evento) => {
+mostrarError = (check, evento) => {
     if(check) {
         evento.classList.add('error');
         errors[evento.name] = true;
