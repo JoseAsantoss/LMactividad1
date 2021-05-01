8d0f748c2130d5b7b60d2888e7923612404cbc93
miguelang611 <miguelang611@gmail.com>
2021-05-01 15:45:38 +0200

    Formulario Cursos v2.2
    Limpieza del fichero JavaScript. Se aprovecha para indicar que todas las versiones v2 del formulario de cursos se deben a la modificación de la sección de cursos, de cara a incluir imágenes en los selectores de cursos, junto con modificaciones via JS del estilo

--- js/formCursos.js
+++ js/formCursos.js
@@ -1,34 +1,3 @@
-/*//Evento seleccionar ingrediente
-function selecCurso(){
-    let sel = false
-    img.addEventListener("click", function () {
-        if (sel == false) {
-            this.setAttribute("style", "filter: brightness(55%) blur(2px);")
-            sel = true
-        } else {
-            this.removeAttribute("style", "filter: brightness(55%) blur(2px);")
-            sel = false
-        }
-}
-*/
-
-/*$("[class = labelCurso]").click(function () {
-    console.log("A")
-    let id = this.id
-    if (id == "cursoNoSeleccionado") {
-        console.log("Vamos a seleccionado")
-        $(this).css({ filter: "brightness(50%) blur(2px)" })
-        $(this).next("#checked").css({ visibility: "visible" })
-        $(this).attr("id", "cursoSeleccionado")
-
-    } else {
-        console.log("Vamos a NOOOO seleccionado")
-        $(this).css({ filter: "brightness(100%) blur(0px)" })
-        $(this).next("#checked").css({ visibility: "hidden" })
-        $(this).attr("id", "cursoNoSeleccionado")
-    }
-})*/
-
 $("[name = cursoForm]").click(function () {
     console.log("A")
     let classDiv = $(this).attr("class");
@@ -53,19 +22,4 @@ $("[name = cursoForm]").click(function () {
         imgLabel.css({ filter: "brightness(100%) blur(0px)" })
         imgCheck.css({ visibility: "hidden" })
     }
-    /*
-    if (classDiv == "noSeleccionado") {
-        $(this).attr("class", "seleccionado")
-        console.log("Vamos a seleccionado")
-        checkBox.prop('checked', true);
-        imgLabel.css({ filter: "brightness(50%) blur(2px)" })
-        imgCheck.css({ visibility: "visible" })
-
-    } else {
-        $(this).attr("class", "noSeleccionado")
-        console.log("Vamos a NOOOO seleccionado")
-        $(this).css({ filter: "brightness(100%) blur(0px)" })
-        $(this).next("#checked").css({ visibility: "hidden" })
-        $(this).attr("id", "cursoNoSeleccionado")
-    }*/
 })
