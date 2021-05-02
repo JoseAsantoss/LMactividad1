10206dc9498e0e7e12e32e68a105c7cd8e3608e4
miguelang611 <miguelang611@gmail.com>
2021-05-02 18:20:41 +0200

    Formulario Cursos v3.3
    Cambios sección cursos. Se generan imágenes provisionales de los cursos seleccionados. Se manejan los cambios necesarios de css via jQuery

--- js/formCursos.js
+++ js/formCursos.js
@@ -1,3 +1,36 @@
+$(document).ready(function(){
+    $("[type = cursoForm]").each(function(){
+        console.log($(this))
+        let divInterno = $(this).children("div")
+        let imgCheck = divInterno.children(".imgCursoChecked")
+        imgCheck.css( "display", "none")
+        console.log(imgCursoCheck)
+    })
+});
+
+$("[type = cursoForm]").click(function () {
+    let classDiv = $(this).attr("class");
+    let div = $(this)
+    let divInterno = div.children("div")
+    let checkBox = div.children("input")
+    let imgNoCheck = divInterno.children(".imgCurso")
+    let imgCheck = divInterno.children(".imgCursoChecked")
+    if(classDiv == "noSeleccionado"){
+        $(this).attr("class", "seleccionado")
+        console.log("Vamos a seleccionado")
+        checkBox.prop('checked', true);
+        imgNoCheck.css("display","none")
+        imgCheck.css( "display", "")
+    }else{
+        $(this).attr("class", "noSeleccionado")
+        console.log("Vamos a NOOOOO seleccionado")
+        checkBox.prop('checked', false);
+        imgNoCheck.css("display","")
+        imgCheck.css( "display", "none")
+    }
+})
+
+/*
 $("[type = cursoForm]").click(function () {
     let classDiv = $(this).attr("class");
     let div = $(this)
@@ -5,13 +38,11 @@ $("[type = cursoForm]").click(function () {
     let checkBox = div.children("input")
     let imgLabel = div.children("div").children(".imgCurso")
     let imgCheck = div.children("div").children(".imgCheck")
-    console.log(imgCheck)
     if(classDiv == "noSeleccionado"){
         $(this).attr("class", "seleccionado")
         console.log("Vamos a seleccionado")
         checkBox.prop('checked', true);
         imgLabel.css({ filter: "brightness(50%) blur(2px)" })
-        //imgCheck.css({ visibility: "visible" })
         let img= $('<img class="imgCheck" src="../images/check.png">')
         img.appendTo(divInterno)
     }else{
@@ -19,17 +50,14 @@ $("[type = cursoForm]").click(function () {
         console.log("Vamos a NOOOOO seleccionado")
         checkBox.prop('checked', false);
         imgLabel.css({ filter: "brightness(100%) blur(0px)" })
-        //imgCheck.css({ visibility: "hidden" })
         imgCheck.remove();
     }
-})
+})*/
 
 $("input[type=radio][name=formacion-previa]").change(function() {
     console.log(this.value)
-    console.log("Ha entrado")
     let divFormacion = $("#otraFormacion")
     let cajaFormacion = divFormacion.children("#personal")
-    let otros = $("#formacOtros")
     if(this.value=="otros") {
         if(cajaFormacion.length == 0){
             console.log("Vamos a poner la caja")
