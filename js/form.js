12fb2f0ad156101dc86354656d7d226418532733
JoseASantoss <j.santos.sanchez@gmail.com>
2021-05-03 23:22:47 +0200

    Establezco las validaciones en los input nombre, apellidos, teléfono y e-mail, dni está pendiente de poner la expresión regular. Falta maquetar bien la zona de formación previa.

--- js/form.js
+++ js/form.js
@@ -3,12 +3,18 @@ const submitButton = document.getElementById('submit-btn');
 let timeout = null;
 let errors = {
     nombre: true,
-    apellidos: true,
-    dni: true,
     email: true,
     telefono: true,
     texto: true,
 };
+
+let errors2 = {
+    nombre: true,
+    apellidos: true,
+    dni: true,
+    email: true,
+    telefono: true,
+}
 
 /*La expresión regular de nombre establece que
 empiece por una letra mayúscula y sigan
@@ -64,7 +70,7 @@ for(i = 0; i<caja.length; i++){
 
 for(j=0; j<caja2.length; j++){
     
-    if(caja2[j].name == 'nombre' || caja2[j].name == 'apellidos' || caja2[j].name == 'dni' || caja2[j].name == 'telefono' || caja2[j].name == 'e-mail'){
+    if(caja2[j].name == 'nombre' || caja2[j].name == 'apellidos' || caja2[j].name == 'dni' || caja2[j].name == 'telefono' || caja2[j].name == 'email'){
         const evento2 = caja2[j];
 
         evento2.addEventListener(
@@ -109,13 +115,15 @@ validation = (evento) => {
         }else{
             mostrarError(false, evento);
         } */
-               
+        mostrarError(false, evento);       
     } 
 
-    if(evento.name == 'email' || evento.name == 'e-mail'){
+    if(evento.name == 'email'){
         if(!evento.value.match(mailformatRegex)){
+            console.log('El e-mail no es correcto');
             mostrarError(true, evento);
         }else{
+            console.log('El e-mail es correcto');
             mostrarError(false, evento);
         }
     }
@@ -131,6 +139,7 @@ validation = (evento) => {
 
     if(evento.name == 'texto') {
         console.log(evento.value);
+        
         if(evento.value.length < 50 || evento.value.length > 1000){
             mostrarError(true, evento);
 
@@ -140,26 +149,49 @@ validation = (evento) => {
         }
     }
     
-    submitControlador();
+    if(evento.name['texto']){
+        submitControlador();
+    }else{
+        submitControlador2();
+    }
+    
+    
 };
 
 mostrarError = (check, evento) => {
     if(check) {
         evento.classList.add('error');
-        errors[evento.name] = true;
+        if(evento.name['texto']){
+            errors[evento.name] = true;
+        }else{
+            errors2[evento.name] = true;
+        }
     }else{
         evento.classList.remove('error');
-        errors[evento.name] = false;
+        if(evento.name['texto']){
+            errors[evento.name] = false;
+        }else{
+            errors2[evento.name] = false;
+        }
     }
 };
 
 submitControlador = () => {
     console.log(errors);
-    if(errors.nombre || errors.apellidos || errors.dni || errors.email || errors.telefono || errors.texto) {
+    if(errors.nombre || errors.apellidos || errors.email || errors.telefono || errors.texto) {
         submitButton.toggleAttribute('disabled', true);
     }else{
         submitButton.toggleAttribute('disabled', false);
     };
 
+}
+
+submitControlador2 = () => {
+    console.log(errors2);
+    if(errors2.nombre || errors2.apellidos || errors2.dni || errors2.email || errors2.telefono) {
+        submitButton.toggleAttribute('disabled', true);
+    }else{
+        submitButton.toggleAttribute('disabled', false);
+    };
 }
 
