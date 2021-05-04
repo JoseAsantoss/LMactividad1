f26cd596fbb4535492588d60e1a424720bc18f4f
miguelang611 <miguelang611@gmail.com>
2021-05-04 22:00:16 +0200

    Mejora Media Queries + limpieza js formulario
    Media Queries corregidas para iPhone X y iPhone 6+/7+/8+ y limpieza código js formulario

--- js/formCursos.js
+++ js/formCursos.js
@@ -30,30 +30,6 @@ $("[type = cursoForm]").click(function () {
     }
 })
 
-/*
-$("[type = cursoForm]").click(function () {
-    let classDiv = $(this).attr("class");
-    let div = $(this)
-    let divInterno = div.children("div")
-    let checkBox = div.children("input")
-    let imgLabel = div.children("div").children(".imgCurso")
-    let imgCheck = div.children("div").children(".imgCheck")
-    if(classDiv == "noSeleccionado"){
-        $(this).attr("class", "seleccionado")
-        console.log("Vamos a seleccionado")
-        checkBox.prop('checked', true);
-        imgLabel.css({ filter: "brightness(50%) blur(2px)" })
-        let img= $('<img class="imgCheck" src="../images/check.png">')
-        img.appendTo(divInterno)
-    }else{
-        $(this).attr("class", "noSeleccionado")
-        console.log("Vamos a NOOOOO seleccionado")
-        checkBox.prop('checked', false);
-        imgLabel.css({ filter: "brightness(100%) blur(0px)" })
-        imgCheck.remove();
-    }
-})*/
-
 $("input[type=radio][name=formacion-previa]").change(function() {
     console.log(this.value)
     let divFormacion = $("#otraFormacion")
