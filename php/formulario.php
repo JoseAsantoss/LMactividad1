105f39efd3f9c41bd27ff30d42763c4081fa008b
JoseASantoss <j.santos.sanchez@gmail.com>
2021-05-05 15:27:39 +0200

    Recepción de datos por post

--- php/formulario.php
+++ php/formulario.php
@@ -1,43 +1,11 @@
-<?php
-$nombre = htmlspecialchars($_POST['nombre']);
-$apellidos = htmlspecialchars($_POST['apellidos']);
-$dni = md5(md5(htmlspecialchars($_POST['dni'])));
-$fecha = htmlspecialchars($_POST['fecha_nacimiento']);
-$domicilio = htmlspecialchars($_POST['domicilio']);
-$cp = htmlspecialchars($_POST['codigo_postal']);
-$localidad = htmlspecialchars('milocalidad');
-$telefono = htmlspecialchars($_POST['telefono']);
-$mail = htmlspecialchars($_POST['email']);
-$formacion = htmlspecialchars($_POST['formacion-previa']);
-if(isset($_POST['enviar'])){
-   if(is_array($_POST['cursos'])){
-    $cursos= '';
-    //Contamos el n�mero de inputs seleccionados con "checked"
-    $checked_cont = count($_POST['cursos']);
-    //Recorremos el Bucle para almacenar y visualizar los valores activos.
-    foreach ($_POST['cursos'] as $key => $seleccion){
-        if($cursos != $checked_cont-1){
-            $cursos .= $value .', ';
-        }else{
-            $cursos .= $seleccion.'.';
-        }
-    }
-}else{
-    $cursos = 'No se ha seleccionado ning�n curso.';
-} 
-}
-
-$modalidad = htmlspecialchars($_POST['modalidad']);
-?>
 <!Doctype html>   
     <html lang="es">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Recepci�n de datos del formulario</title>
+        <title>Recepci&oacuten de datos del formulario</title>
         <link rel="stylesheet" type="text/css" href="../styles/style.css">
-        <link rel="stylesheet" href="../styles/formCursos.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
@@ -51,9 +19,47 @@ $modalidad = htmlspecialchars($_POST['modalidad']);
                 <img class="logo-header" src="../images/logo-svg.svg" alt="logo TERRITORIO WEB">
             </a>
         </header>
-        <main>
-            <section>
-                <ul>
+        <nav class="nav" id="menu">
+            <a href="../index.html" class="activa">Inicio</a>
+            <div class="desplegable">Qué ofrecemos
+                <i class="fa fa-caret-down"></i>
+                <div class="desp-contenido">
+                    <a href="../content/servicios.html">Servicios</a>
+                    <a href="../content/cursos.html">Cursos</a>
+                </div>
+            </div>
+            <a href="../content/about.html">Quiénes somos</a>
+            <a href="../content/contact.html">Contacto</a>
+            <button class="icono" onclick="despliega()">&#9776;</button>
+        </nav>
+<?php
+error_reporting(0);
+$nombre = $_POST['nombre'];
+$apellidos = $_POST['apellidos'];
+$dni = md5(md5(htmlspecialchars($_POST['dni'])));
+$fecha = $_POST['fecha_nacimiento'];
+$domicilio = $_POST['domicilio'];
+$cp = $_POST['codigo_postal'];
+$localidad = $_POST[('milocalidad')];
+$telefono = $_POST['telefono'];
+$mail = $_POST['email'];
+$formacion = $_POST['formacion-previa'];
+$cursos = is_array($_POST['cursos']);
+$checked_cont = count($cursos);
+//Recorremos el Bucle para almacenar y visualizar los valores activos.
+$mostrar = '';
+foreach ($cursos as $key => $seleccion){
+    if($mostrar != $checked_cont-1){
+        $mostrar .= $seleccion .', ';
+    }else{
+        $mostrar .= $seleccion.'.';
+    }
+}
+$modalidad = $_POST['modalidad'];
+?>
+        <main class="main-index">
+            <section class="main-section">
+                <ul class="datos">
                     <h2>Datos recogidos por POST desde el formulario</h2>
                     <li><?php echo 'Nombre: ' . $nombre ?></li>
                     <li><?php echo 'Apellidos: ' . $apellidos?></li>
@@ -98,8 +104,27 @@ $modalidad = htmlspecialchars($_POST['modalidad']);
                 </ul>
             </section>
         </main>
+        <footer>
+            <section class="footer-section-1">
+                <a href="#"><img src="../images/svg/facebook-f-brands.svg" alt="icono facebook" class="icono-redes"></a>
+                <a href="#"><img src="../images/svg/twitter-brands.svg" alt="icono twitter" class="icono-redes"></a>
+                <a href="#"><img src="../images/svg/pinterest-brands.svg" alt="icono pinterest" class="icono-redes"></a>
+                <a href="#"><img src="../images/svg/instagram-brands.svg" alt="icono instagram" class="icono-redes"></a>
+                <a href="#"><img src="../images/svg/youtube-brands.svg" alt="icono youtube" class="icono-redes"></a>
+            </section>
 
+            <section class="footer-section-2">
+                <ul>
+                    <li><a href="#">Términos y condiciones</a></li>
+                    <li><a href="#">Trabaja con nosotros</a></li>
+                    <li><a href="#">Política de privacidad</a></li>
+                </ul>
+            </section>
 
+            <section class="footer-section-3">
+                <p id="copy"></p>
+            </section>
+        </footer>                    
     </body>
     </head>    
 </html>
